b5c90f3cd9fb65bce40ecd4f7d73f3cbe98a4c2d
luhaichuan <luhaichuan@baidu.com>
2017-06-08 12:19:41 +0800

    优化mysql，使用数据库连接池；

--- index.js
+++ index.js
@@ -3,7 +3,15 @@ const mysql = require('mysql')
 const querystring = require('querystring')
 
 // 数据库配置
-const connection = mysql.createConnection({
+// const connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: 'root',
+//     database: 'react_player'
+// })
+
+// 创建连接池
+const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
     password: 'root',
@@ -16,7 +24,7 @@ const port = 3001
 // server监听
 const server = http.createServer((req, res) => {
     // 设置CORS允许
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
+    res.setHeader('Access-Control-Allow-Origin', '*')
     res.setHeader("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
 
@@ -38,18 +46,28 @@ const server = http.createServer((req, res) => {
 
             let result = {}
             const sql = `SELECT * FROM songs LIMIT ${(page - 1)*pageSize},${pageSize}`
-            connection.query(sql, (err, resultSet) => {
+            pool.getConnection((err, connection) => {
                 if (err) {
                     console.log(err)
                 }
-                result.code = 0
-                result.count = pageSize
-                result.playCount = 56899
+                // 使用连接池回调参数查询
+                connection.query(sql, (err, resultSet) => {
+                    console.log('执行SQL')
+                    if (err) {
+                        console.log(err)
+                    }
+                    result.code = 0
+                    result.count = pageSize
+                    result.playCount = 56899
 
-                result.data = resultSet
-                // connection.end()
-                writeResult(res, result)
+                    result.data = resultSet
+                    writeResult(res, result)
+                    // 释放连接池链接
+                    console.log('释放链接')
+                    connection.release()
+                })
             })
+
         })
     }
 })
